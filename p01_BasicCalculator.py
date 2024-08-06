55f9b2814a77bf9b82ac1fe12d9defd371c69996
Petr Gregor <petr.gregor@email.cz>
2024-08-06 20:49:28 +0200

    PyTest: Complex number - zadání.

---
+++ p01_BasicCalculator.py
@@ -0,0 +1,36 @@
+class BasicCalculator:
+
+    def add(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 isinstance(num2, float))):
+            return num1 + num2
+        return None
+
+    def subtract(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 isinstance(num2, float))):
+            return num1 - num2
+        return None
+
+    def multiply(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 isinstance(num2, float))):
+            return num1 * num2
+        return None
+
+    def divide(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 isinstance(num2, float))):
+            try:
+                return num1 / num2
+            except:
+                return None
+        return None
---
+++ p02_ComplexNumber.py
@@ -0,0 +1,21 @@
+"""
+https://cs.wikipedia.org/wiki/Komplexn%C3%AD_%C4%8D%C3%ADslo
+
+Definujte třídu ComplexNumber reprezentující komplexní číslo
+a definujte následující metody:
+- __init__ (konstruktor)
+- __eq__ (rovnost)
+- __lt__ (<)
+- __gt__ (>)
+- __repr__
+- __str__
+- properties (gettery a settery)
+- add (sčítání)
+- subtract (odečítání)
+- multiply (násobení)
+- divide (dělení)
+- conjugate (číslo komplexně sdružené)
+- absolute_value (absolutní hodnota)
+
+A vše řádně otestujte.
+"""
