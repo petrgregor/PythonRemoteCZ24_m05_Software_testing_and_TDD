6a37cf5d4d7e2d079cc56634e76582c7881a49d8
JardaAdam <jardys23@gmail.com>
2024-08-07 22:11:05 +0200

    poznamky a dalsi reseni v : axercise.py, test_exercise.py

--- moje_prace/exercise.py
+++ moje_prace/exercise.py
@@ -1,10 +1,17 @@
-print("exercise1")
+print("exercise2")
 def odd_indexes(string):
     return str(string)[1::2]
 
 print(odd_indexes(123456789))
 
-print("exercise2")
+# Petruv zapis
+if __name__ == "__main__":
+    words = ["elephant", "computer", "Petr", 123456789]
+    for word in words:
+        print(f"{word} -> {odd_indexes(word)}")
+
+
+print("exercise3")
 
 
 
--- moje_prace/test_exercise.py
+++ moje_prace/test_exercise.py
@@ -1,8 +1,43 @@
 import pytest
 
 from exercise import *
+
+
 @pytest.mark.parametrize("string, result", [("highline", "ihie"),
                                             ("policista", "oiit"),
                                             ((123456789), (2, 4, 6, 8))])
 def test_odd_indexes(string, result):
-    assert odd_indexes(string)
+    assert odd_indexes(string) == result
+
+
+# Petrovo reseni
+
+def test_odd_indexes_str():
+    assert odd_indexes("elephant") == "lpat"
+    assert odd_indexes("computer") == "optr"
+    assert odd_indexes("Petr") == "er"
+
+
+def test_odd_indexes_int():
+    assert odd_indexes(123456789) == "2468"
+    assert odd_indexes(3456) == "46"
+    assert odd_indexes(159753) == "573"
+    assert odd_indexes(0) == ""
+    assert odd_indexes(1) == ""
+    assert odd_indexes(11) == "1"
+    assert odd_indexes(111) == "1"
+    assert odd_indexes(1111) == "11"
+
+
+"""Exercise 2a
+
+Rewrite the previous tests to use pytest parameterization and remove code duplication.
+"""
+
+
+# TODO proc ma u inputu (input_) to _ ?
+@pytest.mark.parametrize("input_, result", [("elephant", "lpat"), ("computer", "optr"), ("Petr", "er"),
+                                            (123456789, "2468"), (0, ""), (1, ""), (11, "1"),
+                                            (111, "1"), (1111, "11")])
+def test_odd_indexes_param(input_, result):
+    assert odd_indexes(input_) == result
