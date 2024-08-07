194095f300e005679b304b33888de321d81cc069
JardaAdam <jardys23@gmail.com>
2024-08-07 22:24:56 +0200

    otazky k cviceni 3

--- p05_Exercise_3.py
+++ p05_Exercise_3.py
@@ -6,3 +6,8 @@ if __name__ == "__main__":
     inputs = [12, 11, 2, 4, 123, 92, 79, 19023]
     for input_ in inputs:
         print(f"{input_} -> {func(input_)}")
+
+# TODO vůbec jsme nepochopil zadaní.
+#  Jakto že u posledniho čísla to
+#  napiše když to nasobí každé číslo zvlášt?
+#  chápal bzch vísledek 2,0,6
