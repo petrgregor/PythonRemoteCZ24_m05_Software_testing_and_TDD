b5d4567c17f0b7173176cefc9c7829e4251c7444
JardaAdam <jardys23@gmail.com>
2024-08-08 21:31:29 +0200

    hraju si se skladem exercise5

--- moje_prace/exercise/exercise5.py
+++ moje_prace/exercise/exercise5.py
@@ -13,10 +13,33 @@ class Warehouse:
         sum_od_products = sum([x.volume for x in self.products])
         return self.capacity - sum_od_products
 
-    def add(self, product: Product):
-        rest_space = self.__calculate_free_space()
-        if rest_space >= product.volume:
-            self.products.append(product)
-            return rest_space - product.volume
-        else:
-            return -1
+    def add(self, *products: Product):
+        for product in products:
+            rest_space = self.__calculate_free_space()
+            if rest_space >= product.volume:
+                self.products.append(product)
+                print(f"Položka {product.name} byla přidána do skladu. Zbývající volné místo: {rest_space - product.volume}")
+            else:
+                print(f"Nedostatek místa pro položku {product.name}. Nemohla být přidána.")
+
+
+if __name__ == "__main__":
+    warehouse1 = Warehouse(100)
+
+
+    karabina1 = Product("triplelock", 1  )
+    karabina2 = Product("twistlock", 1)
+    smyce1 = Product("opensling", 1)
+    smyce2 = Product("eye", 1)
+    pulley1 = Product("single_pulley", 1)
+    pulley2 = Product("double_pulley", 1)
+    helmet1 = Product("work_helmet", 2)
+    helmet2 = Product("climbing_helmet", 2)
+    harness1 = Product("work_harness", 3)
+    harness2 = Product("climbing_harness", 2)
+
+    result = warehouse1.add(helmet1,helmet2,harness1)
+
+
+
+
---
+++ moje_prace/exercise/test_exercise5.py
@@ -0,0 +1,7 @@
+# Napiš třídu, která bude představovat zásobník. Kapacita (desetinné hodnoty reprezentující kubické metry)
+# je definována konstruktorem. Vyvtovř rřídu, která bude reflektovat produkt. Objem produktu je určen konstruktorem.
+# Zásobník bude mít metodu "add", která vezme objekt Product jako svůj argument a vrátí jeho zbývající volný prostor
+# nebo -1 pokud do něj nejde přidat předmět z nedostatku prostoru.
+#
+# Použij fixture na přípravu množiny produktů, které se umísťují do skladu každý měsíc a vytvoř testy, které budou
+# kontrolovat jestli sklad správně přidává produkty. Přesnost na dvě desetinná místa.
