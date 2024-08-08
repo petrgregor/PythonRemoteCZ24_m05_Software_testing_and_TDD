72e65a48d6deb443924a8ef479657df4e091df2e
JardaAdam <jardys23@gmail.com>
2024-08-08 18:12:55 +0200

    pridal jsem zapisky do test_coverage

---
+++ moje_prace/test_coverage.py
@@ -0,0 +1,14 @@
+# instalace -> pip install coverage = instalace knihovny
+# prikaz: coverage report *test*.py = vypise v terminalu kolik procent % codu je otestovano
+# coverage report (*test*.py) = v zavorce je cast ktera urcuje ktere soubory bude testovat ->
+# nazev souboru musi obsahovat toto slovo. * = ohraniceni slova
+# Z příkazové řádky pustíme analýzu: coverage run --source="." --omit="*/venv/*" -m pytest
+#                                                | zdroje = vsechny | vynechat =
+# potom vytvoříme report: coverage report product.py company.py
+# výsledek bude vypadat nějak takto:
+# (venv) $ coverage report example.py
+#
+# Name Stmts Miss Cover
+# --------------------------------
+# example.py 9 1 89%
+
